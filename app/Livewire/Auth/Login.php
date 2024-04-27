8d6e56351ff9f0092dfded245a9c8c18ec5916a3
MarcusAfolabi <biormarcus@gmail.com>
2024-04-27 23:36:01 +0100

    updated NIN naming convention

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -68,11 +68,43 @@ class Login extends Component
             Log::alert($th->getMessage());
         }
     }
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+         try {
+            $response = Http::post(ApiEndpoints::login(), [
+                'email' => $this->email,
+                'password' => $this->password,
+                'location_data' => $this->location_data,
+                'device_data' => $this->device_data,
+            ]);
 
+            if ($response->successful()) {
+                $data = $response->json()['user'];
+                $wallet = $response->json()['wallet'];
+                $bonus = $response->json()['bonus'];
+                $token = $response->json()['token'];
+                // Store data and token in session
+                session(['user_data' => $data]);
+                session(['user_wallet' => $wallet]);
+                session(['user_bonus' => $bonus]);
+                session(['user_token' => $token]);
+                info($response->json());
+                return redirect()->to('/dashboard');
+            } else {
+                $errorMessage = $response->json()['message'];
+                info($errorMessage);
+                $this->addError('email', $errorMessage);
+            }
+        } catch (\Exception $e) {
+            info($e->getMessage());
+            $this->addError('email', 'Login failed. Please try again with correct login detail');
+        }
+    }
 
     public function login()
     {
-
+        $this->validate();
         try {
             $response = Http::post(ApiEndpoints::login(), [
                 'email' => $this->email,
---
+++ app/Livewire/VerifyNIN.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class VerifyNIN extends Component
+{
+    public function render()
+    {
+        return view('livewire.verify-nin');
+    }
+}
